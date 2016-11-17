ab77fb1e847799e4c507fa4b367cff0ced24b428
santitamba <santiago.tambasco@gmail.com>
2016-11-17 20:31:33 -0300

    commit
    sonando algo

--- server/api/parser/parser.controller.js
+++ server/api/parser/parser.controller.js
@@ -220,6 +220,17 @@ export function create(req, res) {
     console.log(compasImpl.toString());
 
       var file = new Midi.File();
+    var track = new Midi.Track();
+    file.addTrack(track);
+
+    track.addNote(0, 'c4', 64);
+    track.addNote(0, 'd4', 64);
+    track.addNote(0, 'e4', 64);
+    track.addNote(0, 'f4', 64);
+    track.addNote(0, 'g4', 64);
+    track.addNote(0, 'a4', 64);
+    track.addNote(0, 'b4', 64);
+    track.addNote(0, 'c5', 64);
 
       fs.writeFileSync('test.mid', file.toBytes(), 'binary');
       // ESTO ES PARA GENERAR EL PARSER
